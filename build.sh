216523a867ee1397cf566b66913d4ef180dfd334
Dejan Janic <67026909+dejan1192@users.noreply.github.com>
2023-12-25 09:55:47 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-gcc -o minsweeper game.c -L./lib/ -lraylib -I./include -lm
+if [ ! -L $(pwd)/lib/libraylib.so.450 ]; then
+    ln -s $(pwd)/lib/libraylib.so $(pwd)/lib/libraylib.so.450
+fi
+gcc -o minsweeper game.c -L./lib/  -I./include -lm -Wl,-rpath=./lib/ -lraylib
