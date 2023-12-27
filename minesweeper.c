8e091d85818ebc934a7c981c61a8e273b884c276
dejan1192 <dejan1192@gmail.com>
2023-12-27 19:39:57 +0100

    Removed unused code

--- minesweeper.c
+++ minesweeper.c
@@ -64,68 +64,8 @@ int main() {
         EndDrawing();
     }
     unloadAssets(game.assets);
+    UnloadFont(myfont);
 
     return 0;
 }
 
-#define MAX_NAME_LENGTH 500
-#define MAX_PATH_LENGTH 500
-
-char* strip_whitespace(char* str);
-  char* strip_whitespace(char* str){
-        while( str[0] != '\0' &&  str[0] !='\n' && str[0] != '\t' && str[0] != ' '){
-            str++;
-        }
-        char* dest = str;
-        while(*str){
-            *dest++ = *str++;
-        }
-        *dest = '\0';
-        return dest;
-     }
-     char* removeSpaces(char *str) {
-    int count = 0;
-    for (int i = 0; str[i]; i++) {
-        if (str[i] != ' ') {
-            str[count++] = str[i];
-        }
-    }
-    str[count] = '\0';
-    return str;
-}
-void load_assets_from_file(AssetPath* paths){
-    FILE* fp = fopen("assets.json", "r");
-
-    if(fp == NULL){
-        printf("Error opening file\n");
-        exit(1);
-    }
-
-    int counter = 0;
-
-    char line[MAX_NAME_LENGTH + MAX_PATH_LENGTH + 2]; 
-    while(
-            fgets(line, sizeof(line), fp) != NULL  // while line exists
-            && counter < MAX_ASSETS // && we haven't reached the max number of assets
-        ){
-
-
-
-        char* token = removeSpaces(strtok(line, ":"));
-        
-        if(*token == '{' || *token == '}'){
-            continue;
-        }
-
-        char* name = token;
-        token = strtok(NULL, ":");
-        char* path = token;
-        // TraceLog(LOG_INFO, "Path: %s", *path);
-        paths[counter].name = name;
-        paths[counter].path = path;
-        counter++;
-    }
-     
-   
-
-}
